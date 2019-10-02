0235e737a12c83ac923ceedd15bf06fab0c33a9a
Shubham Awasthi <31537546+awasthishubh@users.noreply.github.com>
2019-10-02 20:47:24 +0530

    Added statement terminators.

--- popup.js
+++ popup.js
@@ -87,20 +87,20 @@ function reset_options(isAuto)
         ERPIITKGP_question2 : document.getElementById('question2').value,
         ERPIITKGP_question3 : document.getElementById('question3').value
     }, function() {
-      if(isAuto) return
+      if(isAuto) return;
       document.getElementById('status').innerHTML = '<div class="alert alert-success" role="alert">Your credentials have been reset. Open <a href="https://erp.iitkgp.ac.in" target="_blank">ERP, IITKGP</a>.</div>';
     });
 }
 
 
-document.getElementById('ERPLoginID').addEventListener('change',()=>save_options(true))
-document.getElementById('ERPPassword').addEventListener('change',()=>save_options(true))
-document.getElementById('answer1').addEventListener('change',()=>save_options(true))
-document.getElementById('answer2').addEventListener('change',()=>save_options(true))
-document.getElementById('answer3').addEventListener('change',()=>save_options(true))
-document.getElementById('question1').addEventListener('change',()=>save_options(true))
-document.getElementById('question2').addEventListener('change',()=>save_options(true))
-document.getElementById('question3').addEventListener('change',()=>save_options(true))
+document.getElementById('ERPLoginID').addEventListener('change',()=>save_options(true));
+document.getElementById('ERPPassword').addEventListener('change',()=>save_options(true));
+document.getElementById('answer1').addEventListener('change',()=>save_options(true));
+document.getElementById('answer2').addEventListener('change',()=>save_options(true));
+document.getElementById('answer3').addEventListener('change',()=>save_options(true));
+document.getElementById('question1').addEventListener('change',()=>save_options(true));
+document.getElementById('question2').addEventListener('change',()=>save_options(true));
+document.getElementById('question3').addEventListener('change',()=>save_options(true));
 
 document.getElementById('save').addEventListener('click',save_options);
 document.getElementById('reset').addEventListener('click',reset_options);
