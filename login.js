bcd8faed7e18889a64a1767e003d10b0e8acd3be
Sahil Shubham <sahil.shubham2000@gmail.com>
2020-09-09 19:30:29 +0530

    added function to prevent infinite loop;

--- login.js
+++ login.js
@@ -1,13 +1,13 @@
 console.log('I am running!');
-var keys = [ 
-	'ERPIITKGP_ERPLoginID', 
-	'ERPIITKGP_ERPPassword', 
-	'ERPIITKGP_answer1', 
-	'ERPIITKGP_answer2', 
-	'ERPIITKGP_answer3', 
-	'ERPIITKGP_question1', 
-	'ERPIITKGP_question2', 
-	'ERPIITKGP_question3' 
+var keys = [
+	'ERPIITKGP_ERPLoginID',
+	'ERPIITKGP_ERPPassword',
+	'ERPIITKGP_answer1',
+	'ERPIITKGP_answer2',
+	'ERPIITKGP_answer3',
+	'ERPIITKGP_question1',
+	'ERPIITKGP_question2',
+	'ERPIITKGP_question3'
 ];
 
 var inputTags = {
@@ -19,13 +19,21 @@ chrome.storage.sync.get(keys, function(authData) {
 	enterData(authData);
 });
 
+function stopper(){
+	if(sessionStorage.getItem("q1")=="1" && sessionStorage.getItem("q2")=="1" && sessionStorage.getItem("q3")=="1"){
+		alert("wrong credentials; Correct your details in the extension and open erp again in new tab/window");
+
+		throw Error("wrong credentials");
+	}
+}
+
 var enterData = function(authData) {
 	inputTags['ERPIITKGP_ERPLoginID'].value = authData['ERPIITKGP_ERPLoginID'];
 	inputTags['ERPIITKGP_ERPPassword'].value = authData['ERPIITKGP_ERPPassword'];
 	inputTags['ERPIITKGP_ERPLoginID'].blur();
-	
+
 	var xhttp = new XMLHttpRequest()
-	
+
 	xhttp.onreadystatechange = function answerSecurityQuestion() {
 		if(this.readyState == 4 && this.status == 200) {
 			var securityQuestion 		= this.responseText;
@@ -34,18 +42,22 @@ var enterData = function(authData) {
 
 			securityQuestionInput.innerHTML = securityQuestion;
 			document.getElementById("answer_div").className = document.getElementById("answer_div").className.replace(/\bhidden\b/,'');
-			
+
 			if(securityQuestion == authData['ERPIITKGP_question1']){
 				answerInput.value = authData['ERPIITKGP_answer1'];
+				sessionStorage.setItem("q1", "1");
 			} else if(securityQuestion == authData['ERPIITKGP_question2']){
 				answerInput.value = authData['ERPIITKGP_answer2'];
+				sessionStorage.setItem("q2", "1");
 			} else if(securityQuestion == authData['ERPIITKGP_question3']){
 				answerInput.value = authData['ERPIITKGP_answer3'];
+				sessionStorage.setItem("q3", "1");
 			}
 
 			document.forms[0].submit();
     }
 	}
+	stopper();
 
 	xhttp.open("POST", "https://erp.iitkgp.ac.in/SSOAdministration/getSecurityQues.htm", true);
 	var params="user_id=" + authData['ERPIITKGP_ERPLoginID'];
