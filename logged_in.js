980be3bfd8a20e68702aea8062d6e51d93c8e254
Sahil Shubham <sahil.shubham2000@gmail.com>
2020-09-10 19:10:39 +0530

    fixes #27 (#28) fixes #13

--- logged_in.js
+++ logged_in.js
@@ -3,7 +3,9 @@ setTimeout(skipPopup, 2000);
 setTimeout(skipPopup, 3000);
 setTimeout(skipPopup, 4000);
 
+sessionStorage.clear();
+
 function skipPopup() {
     console.log("Trying to skip popup");
-    document.getElementById("skiplink").click();  
+    document.getElementById("skiplink").click();
 }
