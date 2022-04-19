d783e5cfc8feb848d4bc94fdc5ff555917a5bc97
rajivharlalka <rajivharlalka009@gmail.com>
2022-04-19 13:30:58 +0530

    Removed Popup Script

--- skip-form.js
+++
@@ -1,7 +0,0 @@
-// Original Author:
-// https://github.com/rohitner/
-// Skips ERP annoying form that pops up
-// Pop Up removed from erp , commented for future Reference and Use
-// window.setTimeout(() => {
-//     document.getElementById("skiplink").click();
-// }, 5000);
